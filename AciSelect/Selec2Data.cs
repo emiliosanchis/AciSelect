db403de4b7ea5735e506b9eca7108928b360449e
emiliosanchis <52755485+emiliosanchis@users.noreply.github.com>
2019-07-31 20:21:47 +0200

    Select2 implementation

---
+++ AciSelect/Selec2Data.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AciSelect
+{
+    public class Selec2Data
+    {
+        public Selec2Data(dynamic Value, string Text)
+        {
+            this.Text = Text;
+            this.Value = Value;
+        }
+
+        public Selec2Data(dynamic Value, string Text, bool Selected)
+        {
+            this.Text = Text;
+            this.Value = Value;
+            this.Selected = Selected;
+        }
+
+        public dynamic Value { get; set; }
+        public string Text { get; set; }
+        public bool Selected { get; set; } = false;
+    }
+}
---
+++ AciSelect/wwwroot/common.js
@@ -0,0 +1,39 @@
+﻿var LastElementDOM;
+var LastValueDOM;
+
+function mutateDOM(ElementDOM, value) {
+    if (LastElementDOM === ElementDOM && LastValueDOM === value)
+        return false;
+
+    LastElementDOM = ElementDOM;
+    LastValueDOM = value;
+
+    var event = new Event('input', {
+        'bubbles': true,
+        'cancelable': true
+    });
+    ElementDOM.dispatchEvent(event);
+}
+
+function selectChange(_this) {
+    mutateDOM(_this, _this.value);
+}
+
+window.JsFunctions = {
+
+    initializeSelect2: function (name, placeholder) {
+        if (placeholder === null || placeholder === '') {
+            $('#' + name).select2({
+                allowClear: false
+            });
+        }
+        else {
+            $('#' + name).select2({
+                placeholder: 'Seleccione una opción',
+                allowClear: false
+            });
+        }
+        return true;
+    }
+
+};
